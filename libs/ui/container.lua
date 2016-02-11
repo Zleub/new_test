ea21acd06588ca31987a5de83ff074539240371f
Zleub <debray.arnaud@gmail.com>
2016-02-11 20:02:43 +0100

    ui base

---
+++ libs/ui/container.lua
@@ -0,0 +1,25 @@
+--          `--::-.`
+--      ./shddddddddhs+.
+--    :yddddddddddddddddy:
+--  `sdddddddddddddddddddds`
+--  ydddh+sdddddddddy+ydddds  new_test:container
+-- /ddddy:oddddddddds:sddddd/ By adebray - adebray
+-- sdddddddddddddddddddddddds
+-- sdddddddddddddddddddddddds Created: 2016-02-11 17:15:43
+-- :ddddddddddhyyddddddddddd: Modified: 2016-02-11 17:20:26
+--  odddddddd/`:-`sdddddddds
+--   +ddddddh`+dh +dddddddo
+--    -sdddddh///sdddddds-
+--      .+ydddddddddhs/.
+--          .-::::-`
+
+local Container = Class:expand()
+
+function Container:create()
+	local c = Class.create(self)
+
+
+end
+
+
+return {}
